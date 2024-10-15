1c4f35d2ff06f0855300702b77c7bc7e52436f2d
K.M.SATHYANANDA REDDY <sathyananda.kethireddy@gmail.com>
2024-10-15 22:32:53 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SIZE_STACK 50
+
+#include <stdio.h> 
+#include <ctype.h>
+
+typedef struct{
+    int top;
+    int stack[SIZE_STACK];
+}Stack_t;
+
+int Infix_Postfix_conversion(char *Postfix_exp, char *Infix_exp, Stack_t *stk);
+int Postfix_Eval(char *Postfix_exp, Stack_t *stk);
+int priority(char opr);
+void push(Stack_t *stk, int data);
+int peek(Stack_t *stk);
+void pop(Stack_t *stk);
+#endif
